3ac042dda6a73a033553e464139b3bcfea47b8b9
j96.king <j96.king@JTTKing14>
2016-07-28 13:56:18 -0400

    log in, registration, and confirmation

--- index.php
+++ index.php
@@ -7,15 +7,27 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Registration</title>
+        <title>Bookazon Sign IN</title>
     </head>
     <body>
+        <h1>Login</h1>
+        <p>Username:</p>
+        <p>Password:</p>
+       <button type="button" onclick="alert('Hello world!')">Sign in</button>
     
-        <h1>Registration Page</h1>
+        <h1>Registration</h1>
         <p>First Name:</p>
            <p>Last Name:</p>
            <p>Email address:</p>
            <p>Phone Number:</p>
            <p>Billing Address:</p>
+           <button type="button" onclick="alert('Hello world!')">Finish</button>
+           
+           <h1>Confirmation</h1>
+        <p>Username:</p>
+        <p>Password:</p>
+        <p>re-enter password</p>
+       <button type="button" onclick="alert('Hello world!')">Confirm</button>
+
     </body>
 </html>
