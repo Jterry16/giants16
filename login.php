6dafc0b3c7c2ce8aae9cfa17b6e8fdd05012f4fd
j96.king <j96.king@JTTKing14>
2016-07-28 16:52:08 -0400

    log in, registration, and confirmation

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        ?>
+
+<h1>Please log in</h1>
+
+<form action="login.php">
+
+UserName:<br>
+<input type="text" name="username">
+<br>
+
+Password:<br>
+<input type="text" name="lastname">
+<br><br>
+<input type="submit" value="Log in">
+
+
+</form>
+    </body>
+</html>
